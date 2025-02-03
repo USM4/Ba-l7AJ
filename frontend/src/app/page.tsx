e66a89c02227c16badb7decf770b0d1f1dc7038e
USM4 <oussamaredoine06@gmail.com>
2025-02-03 18:29:25 +0100

    added some global colors for simplifiyng the tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] text-red-300 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcome to Jotex
+    <div className="flex justify-center items-center text-7xl font-mono  text-secondary  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      TEST TEST
     </div>
   );
 }
